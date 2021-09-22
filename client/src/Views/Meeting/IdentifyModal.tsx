fc447969b943baaf678c288aada1aec5519c36b8
robertnowell <robert.j.nowell@gmail.com>
2021-09-22 13:45:21 -0700

    gcal integration

--- client/src/Views/Meeting/IdentifyModal.tsx
+++ client/src/Views/Meeting/IdentifyModal.tsx
@@ -1,29 +1,138 @@
-import React from "react";
+import {useState, useEffect} from 'react';
+declare global {
+  interface Window {
+    gapi: any;
+  }
+}
 
 export function IdentifyModal() {
+  const CLIENT_ID =
+    '59554851138-objj75qdgbinjf47pit1rh57ebpkf7vt.apps.googleusercontent.com';
+  const API_KEY = 'AIzaSyCcJ2q-DhpLdjZH1URnzZLU9Jmna36SNog';
+  const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
+  const [events, setEvents] = useState<any>([]);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.async = true;
+    script.defer = true;
+    script.src = 'https://apis.google.com/js/api.js';
+
+    document.body.appendChild(script);
+
+    // script.addEventListener('load', () => {
+    //   if (window.gapi) handleClientLoad();
+    // });
+  }, []);
+
+  useEffect(() => {
+    console.log('events: ', events);
+  }, [events]);
+
+  const handleClientLoad = () => {
+    window.gapi.load('client:auth2', initClient);
+  };
+
+  const openSignInPopup = () => {
+    window.gapi.auth2.authorize(
+      {client_id: CLIENT_ID, scope: SCOPES},
+      (res: {access_token: string}) => {
+        console.log('res', res);
+        if (res.access_token) {
+          localStorage.setItem('access_token', res.access_token);
+
+          // Load calendar events after authentication
+          window.gapi.client.load('calendar', 'v3', listUpcomingEvents);
+        }
+      },
+    );
+  };
+
+  const initClient = () => {
+    if (!localStorage.getItem('access_token')) {
+      openSignInPopup();
+    } else {
+      // Get events if access token is found without sign in popup
+      console.log('also here');
+      fetch(
+        `https://www.googleapis.com/calendar/v3/calendars/primary/events?key=${API_KEY}&orderBy=startTime&singleEvents=true`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+          },
+        },
+      )
+        .then((res) => {
+          // Check if unauthorized status code is return open sign in popup
+          if (res.status !== 401) {
+            return res.json();
+          } else {
+            localStorage.removeItem('access_token');
+            openSignInPopup();
+          }
+        })
+        .then((data) => {
+          if (data?.items) {
+            setEvents(formatEvents(data.items));
+          }
+        });
+    }
+  };
+
+  const listUpcomingEvents = () => {
+    window.gapi.client.calendar.events
+      .list({
+        // Fetch events from user's primary calendar
+        calendarId: 'primary',
+        showDeleted: true,
+        singleEvents: true,
+      })
+      .then(function (response: {result: {items: any}}) {
+        let events = response.result.items;
+
+        if (events.length > 0) {
+          setEvents(formatEvents(events));
+        }
+      });
+  };
+
+  const formatEvents = (list: any[]) => {
+    return list.map(
+      (item: {
+        summary: any;
+        start: {dateTime: any; date: any};
+        end: {dateTime: any; date: any};
+      }) => ({
+        title: item.summary,
+        start: item.start.dateTime || item.start.date,
+        end: item.end.dateTime || item.end.date,
+      }),
+    );
+  };
+
   return (
     <div>
       <header
         style={{
-          position: "relative",
+          position: 'relative',
           zIndex: 3,
-          backgroundColor: "white",
+          backgroundColor: 'white',
           opacity: 1,
-        }}
-      >
-        <h1>hello</h1>
+        }}>
+        <button type="submit" onClick={handleClientLoad}>
+          Log in
+        </button>
       </header>
       <div
         style={{
-          position: "fixed",
-          width: "100vw",
-          height: "100vh",
-          backgroundColor: "black",
+          position: 'fixed',
+          width: '100vw',
+          height: '100vh',
+          backgroundColor: 'black',
           left: 0,
           top: 0,
           opacity: 0.5,
-        }}
-      ></div>
+        }}></div>
     </div>
   );
 }
